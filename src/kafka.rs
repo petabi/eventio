b935a349622a178fe3ce149521027a84b28282b7
Min Kim <msk@dolbo.net>
2019-10-11 14:49:55 -0700

    Add error handling for Input (#4)

--- src/kafka.rs
+++ src/kafka.rs
@@ -1,8 +1,8 @@
 //! Reading/writing events from/to Apache Kafka servers.
 
 use crate::fluentd::{Entry, ForwardMode};
+use crate::Error;
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
-use kafka::error::{Error, ErrorKind};
 use kafka::producer::{Producer, Record, RequiredAcks};
 use rmp_serde::Serializer;
 use serde::Serialize;
@@ -16,9 +16,9 @@ pub struct Event {
 
 #[derive(Debug)]
 pub struct EntryLocation {
-    pub remainder: u32, // # of entries in the message after this entry
-    pub partition: i32,
-    pub offset: i64,
+    remainder: u32, // # of entries in the message after this entry
+    partition: i32,
+    offset: i64,
 }
 
 /// Event reader for Apache Kafka.
@@ -36,7 +36,7 @@ impl Input {
         group: String,
         client_id: String,
         topic: String,
-    ) -> Result<Self, Error> {
+    ) -> Result<Self, kafka::Error> {
         let consumer = Consumer::from_hosts(hosts)
             .with_group(group)
             .with_fallback_offset(FetchOffset::Earliest)
@@ -56,10 +56,13 @@ impl Input {
         let data_channel = if let Some(channel) = &self.data_channel {
             channel
         } else {
-            return Err(Error::from_kind(ErrorKind::Msg("channel closed".into())));
+            return Err(Error::ChannelClosed);
         };
 
-        let messagesets = self.consumer.poll()?;
+        let messagesets = self
+            .consumer
+            .poll()
+            .map_err(|e| Error::CannotFetch(Box::new(e)))?;
         if messagesets.is_empty() {
             return Ok(());
         }
@@ -67,19 +70,13 @@ impl Input {
         let mut sel = crossbeam_channel::Select::new();
         let send_data = sel.send(data_channel);
         let recv_ack = sel.recv(&self.ack_channel);
-        for msgset in messagesets.iter() {
+        'messagesets: for msgset in messagesets.iter() {
             let partition = msgset.partition();
             for msg in msgset.messages() {
-                let fwd_msg = (rmp_serde::from_slice(msg.value)
-                    as Result<ForwardMode, rmp_serde::decode::Error>)
-                    .map_err(|e| {
-                        Error::from_kind(ErrorKind::Msg(format!(
-                            "invalid MessagePack message: {}",
-                            e
-                        )))
-                    })?;
+                let fwd_msg: ForwardMode = rmp_serde::from_slice(msg.value)
+                    .map_err(|e| Error::InvalidMessage(Box::new(e)))?;
                 if fwd_msg.entries.len() > u32::max_value() as usize {
-                    return Err(Error::from_kind(ErrorKind::Msg("too many entires".into())));
+                    return Err(Error::TooManyEvents(fwd_msg.entries.len()));
                 }
                 let offset = msg.offset;
                 for (remainder, entry) in (0..fwd_msg.entries.len()).rev().zip(fwd_msg.entries) {
@@ -97,30 +94,34 @@ impl Input {
                                         offset,
                                     },
                                 };
-                                oper.send(data_channel, event).map_err(|e| {
-                                    Error::from_kind(ErrorKind::Msg(format!(
-                                        "cannot send message through channel: {}",
-                                        e
-                                    )))
-                                })?;
+                                if oper.send(data_channel, event).is_err() {
+                                    // data_channel was disconnected. Exit the
+                                    // loop and commit consumed.
+                                    break 'messagesets;
+                                }
                                 break;
                             }
                             i if i == recv_ack => {
-                                let ack = oper.recv(&self.ack_channel).map_err(|e| {
-                                    Error::from_kind(ErrorKind::Msg(format!(
-                                        "cannot receive message through channel: {}",
-                                        e
-                                    )))
-                                })?;
+                                let ack = if let Ok(ack) = oper.recv(&self.ack_channel) {
+                                    ack
+                                } else {
+                                    // ack_channel was disconnected. Exit the
+                                    // loop and commit consumed.
+                                    break 'messagesets;
+                                };
                                 if ack.remainder == 0 {
-                                    self.consumer.consume_message(
-                                        msgset.topic(),
-                                        ack.partition,
-                                        ack.offset,
-                                    )?;
+                                    self.consumer
+                                        .consume_message(msgset.topic(), ack.partition, ack.offset)
+                                        .map_err(|_| {
+                                            Error::Fatal(
+                                                "messages from Kafka have different topics".into(),
+                                            )
+                                        })?;
                                 }
                                 if self.ack_channel.is_empty() {
-                                    self.consumer.commit_consumed()?;
+                                    self.consumer
+                                        .commit_consumed()
+                                        .map_err(|e| Error::CannotCommit(Box::new(e)))?;
                                 }
                             }
                             _ => unreachable!(),
@@ -140,10 +141,15 @@ impl Input {
         for ack in &self.ack_channel {
             if ack.remainder == 0 {
                 self.consumer
-                    .consume_message(&topic, ack.partition, ack.offset)?;
+                    .consume_message(&topic, ack.partition, ack.offset)
+                    .map_err(|_| {
+                        Error::Fatal("messages from Kafka have different topics".into())
+                    })?;
             }
         }
-        self.consumer.commit_consumed()?;
+        self.consumer
+            .commit_consumed()
+            .map_err(|e| Error::CannotCommit(Box::new(e)))?;
         Ok(())
     }
 }
@@ -163,7 +169,7 @@ where
         data_channel: crossbeam_channel::Receiver<T>,
         hosts: Vec<String>,
         topic: String,
-    ) -> Result<Self, Error> {
+    ) -> Result<Self, kafka::Error> {
         let producer = Producer::from_hosts(hosts)
             .with_required_acks(RequiredAcks::One)
             .create()?;
@@ -174,11 +180,14 @@ where
         })
     }
 
-    pub fn run(&mut self) -> Result<(), Error> {
+    pub fn run(&mut self) -> Result<(), kafka::Error> {
         let mut buf = Vec::new();
         for msg in self.data_channel.iter() {
             msg.serialize(&mut Serializer::new(&mut buf)).map_err(|e| {
-                Error::from_kind(ErrorKind::Msg(format!("cannot serialize: {}", e)))
+                kafka::Error::from_kind(kafka::error::ErrorKind::Msg(format!(
+                    "cannot serialize: {}",
+                    e
+                )))
             })?;
             self.producer
                 .send(&Record::from_value(&self.topic, buf.as_slice()))?;
