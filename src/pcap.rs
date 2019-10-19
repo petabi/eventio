cece4308fb3bcc22c68175118b5df14ceaa2d6b9
Min Shao <mshao@petabi.com>
2019-10-18 18:23:53 -0700

    handling pcap

--- src/pcap.rs
+++ src/pcap.rs
@@ -5,7 +5,10 @@ use std::fs::File;
 use std::io::{self, Read};
 use std::path::Path;
 
-use pcap_parser::{create_reader, traits::PcapReaderIterator};
+use pcap_parser::{
+    create_reader, data::get_packetdata_ethernet, data::PacketData, traits::PcapReaderIterator,
+    Block, PcapBlockOwned, PcapError,
+};
 
 #[derive(Debug)]
 pub struct Event {
@@ -63,6 +66,74 @@ impl<'a, R: Read + 'a> super::Input for Input<'a, R> {
         let recv_ack = sel.recv(&self.ack_channel);
         let mut id = 0;
 
+        'poll: loop {
+            match self.iter.next() {
+                Ok((offset, block)) => {
+                    let res = match block {
+                        PcapBlockOwned::NG(Block::SectionHeader(ref _shb)) => None,
+                        PcapBlockOwned::NG(Block::InterfaceDescription(ref _idb)) => None,
+                        PcapBlockOwned::NG(Block::EnhancedPacket(ref epb)) => {
+                            get_packetdata_ethernet(epb.data, epb.caplen as usize)
+                        }
+                        PcapBlockOwned::NG(Block::SimplePacket(ref spb)) => {
+                            get_packetdata_ethernet(spb.data, (spb.block_len1 - 16) as usize)
+                        }
+                        PcapBlockOwned::NG(_) => None,
+                        PcapBlockOwned::Legacy(lpb) => {
+                            get_packetdata_ethernet(lpb.data, lpb.caplen as usize)
+                        }
+                        PcapBlockOwned::LegacyHeader(_) => None,
+                    };
+
+                    if let Some(PacketData::L2(eslice)) = res {
+                        id += 1;
+                        loop {
+                            let oper = sel.select();
+                            match oper.index() {
+                                i if i == send_data => {
+                                    let event = Event {
+                                        raw: eslice.to_vec(),
+                                        id,
+                                    };
+                                    if oper.send(data_channel, event).is_err() {
+                                        // data_channel was disconnected. Exit the
+                                        // loop and commit consumed.
+                                        break 'poll;
+                                    }
+                                    break;
+                                }
+                                i if i == recv_ack => {
+                                    if oper.recv(&self.ack_channel).is_err() {
+                                        // ack_channel was disconnected. Exit the
+                                        // loop and commit consumed.
+                                        break 'poll;
+                                    };
+                                }
+                                _ => unreachable!(),
+                            }
+                        }
+                    }
+                    self.iter.consume_noshift(offset);
+                }
+                Err(PcapError::Eof) => break 'poll,
+                Err(PcapError::Incomplete) => {
+                    self.iter.refill().map_err(|e| {
+                        Error::CannotFetch(Box::new(io::Error::new(
+                            io::ErrorKind::Other,
+                            format!("cannot read packet from pcap: {:?}", e),
+                        )))
+                    })?;
+                }
+                Err(e) => {
+                    return Err(Error::CannotFetch(Box::new(io::Error::new(
+                        io::ErrorKind::Other,
+                        format!("cannot read packet from pcap: {:?}", e),
+                    ))));
+                }
+            }
+        }
+        self.data_channel = None;
+        for _ in &self.ack_channel {}
         Ok(())
     }
 }
