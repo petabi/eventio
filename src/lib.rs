6c40c8da205a3e77a8fcbbec8d177dac23c9265b
Min Kim <msk@dolbo.net>
2019-11-01 16:33:55 -0700

    Bump version to 0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 //! Channel-based event I/O processors.
 //!
-//! This crate allows to collect events from Apache Kafka servers, distribute
-//! them to multiple threads, and optionally collect them to send to Kafka.
+//! This crate allows to collect events from text files, pcap files, and Apache
+//! Kafka servers, distribute them to multiple threads, and optionally collect
+//! them to send to Kafka.
 
 pub mod fluentd;
 pub mod kafka;
